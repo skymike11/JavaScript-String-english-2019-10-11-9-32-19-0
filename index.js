4bdaa9998fc401055044a232ff009276562fe041
skymike <skymike11@qq.com>
2020-06-23 11:00:26 +0800

    提交

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+console.log(name.toUpperCase())
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+console.log(sentence.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase()))
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+console.log(money.replace(/[^0-9]/ig,""))
